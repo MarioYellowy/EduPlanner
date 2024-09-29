8c213322cc440db65245f780e7eb438d72741c8a
Mario <mario03eche@gmail.com>
2024-09-29 13:58:48 -0600

    changes

--- src/main.js
+++ src/main.js
@@ -1,25 +1,17 @@
-const { BrowserWindow } = require('electron')
-
-let window;
-function hola() {
-    console.log('holis')
-};
+const {BrowserWindow} = require('electron')
+let window
 
 function createWindow() {
     window = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1920,
+        height: 1280,
         webPreferences: {
             nodeIntegration: true
         }
     })
-
     window.loadFile('src/ui/index.html')
-
-
 }
-
+    
 module.exports = {
-    createWindow,
-    hola
-};
+    createWindow
+}
