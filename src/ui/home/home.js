232bee1e52991ea8c5d8516e5a43069e420041a8
egecarli <carla.gonzalez@jala.university>
2024-10-10 19:13:06 -0300

    delete coments

--- src/ui/home/home.js
+++ src/ui/home/home.js
@@ -1,11 +1,10 @@
 const { ipcRenderer } = require('electron');   
 let currentUserId;   
 
-// Este listener se define para recibir el ID de usuario  
 ipcRenderer.on('set-user-id', (event, userId) => {  
     currentUserId = userId;   
     console.log('ID de usuario recibido en home:', currentUserId);  
-    useCurrentUserId(); // Llama a la función después de recibir el ID  
+    useCurrentUserId(); 
 });  
 
 function useCurrentUserId() {  
@@ -15,20 +14,3 @@ function useCurrentUserId() {
         console.log('currentUserId aún no está definido.');  
     }  
 }  
-
-// Inicialmente, no es necesario llamar a useCurrentUserId() aquí,  
-// ya que currentUserId puede ser undefined.
-// Puedes llamar a esta función en respuesta a otros eventos o acciones
-
-// Aquí puedes usar `currentUserId` donde lo necesites dentro de `home.js`
-
-/*document.getElementById('logout-btn').addEventListener('click', function(event) {
-    const confirmLogout = window.confirm('¿Sure you want to logout?');
-    if (!confirmLogout) {
-        event.preventDefault(); 
-    } else {
-        console.log("Cerrando sesión...");
-      
-        window.location.href = "../login/login.html";
-    }
-});*/
