1cece0d533830493099213eb594341a1dd48475e
egecarli <carla.gonzalez@jala.university>
2024-10-12 21:35:21 -0300

    get subject from user

--- src/ui/home/home.js
+++ src/ui/home/home.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const btn_addSubject = document.querySelector(".add-subject-btn");
     const span = document.getElementById("closeModal");
     const modal = document.getElementById('addSubjectModal');
-    const sidebarMenu = document.getElementById("sidebar"); 
+    const sidebarMenu = document.getElementById("sidebar");
     const sidepanel = document.getElementById("sidepanel");
     const btn_logout = document.getElementById("logoutBtn");
 
@@ -14,11 +14,11 @@ document.addEventListener("DOMContentLoaded", () => {
         ipcRenderer.send('logout');
     });
 
-    ipcRenderer.on('set-user-id', (event, userId) => {
+    ipcRenderer.on('set-user-id', async (event, userId) => {
         currentUserId = userId;
         console.log('ID de usuario recibido en home:', currentUserId);
         useCurrentUserId();
- 
+
         btn_addSubject.addEventListener('click', async () => {
             if (modal) {
                 await openAddSubject(modal);
@@ -26,58 +26,88 @@ document.addEventListener("DOMContentLoaded", () => {
                 console.error('Modal no encontrado');
             }
         });
+
         span.onclick = function () {
             modal.classList.remove("show");
         }
+
+
+        await loadAllSubjects(currentUserId);
     });
 
     sidebarMenu.addEventListener('click', () => {
-        toggleMenu(); 
+        toggleMenu();
     });
 
     function toggleMenu() {
-        sidepanel.classList.toggle("open"); 
+        sidepanel.classList.toggle("open");
     }
 
-    function useCurrentUserId() {  
-        if (currentUserId) {  
-            console.log('Usando ID de usuario:', currentUserId);  
-        } else {  
-            console.log('currentUserId aún no está definido.');  
-        }  
-    }  
+    function useCurrentUserId() {
+        if (currentUserId) {
+            console.log('Usando ID de usuario:', currentUserId);
+        } else {
+            console.log('currentUserId aún no está definido.');
+        }
+    }
 
     async function addSubject(id_user, subjectName, subjectDateStart, subjectDateEnd) {
         const conn = await getConnection();
         try {
             await conn.query('CALL addSubject(?,?,?,?,?)', [id_user, subjectName, subjectDateStart, subjectDateEnd, 1]);
             console.log('Nueva materia agregada:', subjectName, 'Fecha inicio:', subjectDateStart, 'Fecha fin:', subjectDateEnd);
-            return true; 
+            return true;
         } catch (error) {
             console.error('Error al agregar la materia:', error);
-            return false; 
+            return false;
         }
     }
 
     function openAddSubject(modal) {
         return new Promise((resolve) => {
-            modal.classList.add("show"); 
+            modal.classList.add("show");
             document.getElementById("addSubjectForm").onsubmit = async function (event) {
                 event.preventDefault();
-    
+
                 let subjectName = document.getElementById('subjectName').value;
                 let subjectDateStart = document.getElementById('subjectDate').value;
                 let subjectDateEnd = document.getElementById('subjectDateEnd').value;
-    
+
                 let success = await addSubject(currentUserId, subjectName, subjectDateStart, subjectDateEnd);
                 if (success) {
-                    event.target.reset(); 
-                    modal.classList.remove("show"); 
+                    event.target.reset();
+                    modal.classList.remove("show");
+                    await loadAllSubjects(currentUserId);
                 } else {
                     alert('Error al agregar la materia. Por favor, inténtalo de nuevo.');
                 }
             };
             resolve();
         });
+    }
+
+    async function getAllSubjects(user_id) {
+        const conn = await getConnection();
+        try {
+            const result = await conn.query('CALL getUserSubjects(?)', [user_id]);
+            const listOfSubject = result[0][0];
+            return listOfSubject;
+        } catch (error) {
+            console.error('Error al obtener las materias:', error);
+            throw error;
+        } finally {
+            if (conn) {
+                conn.end();
+            }
+        }
+    }
+
+    async function loadAllSubjects(userId) {
+        try {
+            const allSubjects = await getAllSubjects(userId);
+            console.log('Materias obtenidas:', allSubjects);
+        } catch (error) {
+            console.error('Error al obtener las materias:', error);
+        }
     }
 });
